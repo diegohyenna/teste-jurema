854558b49cbac0e26e4f8ffe2599d43c7e596742
diegohyenna <dgmartins87@hotmail.com>
2019-11-27 14:02:42 -0300

    upar para heroku

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static(`$/dist/$`));
+app.use(express.static(`$/dist/teste-jurema$`));
  
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`$/dist/$/index.html`));
+res.sendFile(path.join(`$/dist/teste-jurema/index.html`));
 });
  
 app.listen(process.env.PORT || 8080);
